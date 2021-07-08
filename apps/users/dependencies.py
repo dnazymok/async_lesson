a16ddb69ba2cef2a4ba96cece696ec045570b735
Dmitriy_Nazymok <dnazymok@gmail.com>
2021-07-08 19:57:48 +0300

    second

--- apps/users/dependencies.py
+++ apps/users/dependencies.py
@@ -40,6 +40,16 @@ class JSONPlaceholderUserRepository(UserRepository):
         raw_users = await self._list_users()
         return [self._convert_user(raw_users) for raw_user in raw_users]
 
+    async def create_user(self, params: CreateUserParams) -> User:
+        raw_user = await self._create_user(params)
+        return self._convert_user(raw_user)
+
+    async def _create_user(self, params):
+        async with aiohttp.ClientSession() as session:
+            resp = await session.post(self._endpoint, json=params.dict())
+            raw_user = await resp.json()
+            return raw_user
+
     async def _list_users(self):
         async with aiohttp.ClientSession as session:
             resp = await session.get(self._endpoint)
@@ -56,7 +66,7 @@ class UserRepositoryFactory:
 
     def __call__(self):
         if self._repo is None:
-            self._repo = UserRepository()
+            self._repo = JSONPlaceholderUserRepository()
         return self._repo
 
 
